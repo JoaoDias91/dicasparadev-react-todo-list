9ef9f58ff3153abc4512ef44d531cee2df4feeae
Joao <joaodiassiqueira@gmail.com>
2021-12-27 15:23:12 -0300

    Add icon close e Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import {v4 as uuidv4 } from 'uuid';
-import Tasks from "./components/Tasks";
+
 import './App.css';
+
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 
@@ -40,11 +43,19 @@ const App = () => {
     setTasks(newTask);
   }
 
+  const handleTaskDeletion = (taskId) => {
+      const newTasks = tasks.filter(task => task.id !== taskId);
+      setTasks(newTasks);
+  }
+
   return (
       <>
         <div className="container"> 
+          <Header></Header>
           <AddTask handleTaskAddition={handleTaskAddition}/>
-          <Tasks tasks={tasks}  handleTaskClick={handleTaskClick}/>
+          <Tasks tasks={tasks}  
+            handleTaskClick={handleTaskClick} 
+            handleTaskDeletion={handleTaskDeletion}/>
         </div>
       </>
   );
