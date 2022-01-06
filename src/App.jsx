d331f32a854615b4f91951a9b897aa141c03296a
Joao paulo Dias Siqueira <joao@MacBook-Air-de-Joao.local>
2022-01-06 10:07:47 -0300

    Add chamada de api para completar tasks e ajuste de buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {v4 as uuidv4 } from 'uuid';
+import axios  from "axios";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import './App.css';
@@ -25,6 +26,16 @@ const App = () => {
       completed: true,
     },
   ]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const {data} = await axios.get(
+        "https://jsonplaceholder.cypress.io/todos?_limit=10"
+      );
+      setTasks(data);
+    };
+    fetchTasks();
+  }, []);
 
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map(task =>{
